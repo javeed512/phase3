09d07b9b4042fe7950039f034641562092f1c5d5
javeed512 <javeedmohammedjaveed@gmail.com>
2022-03-22 16:20:09 +0530

    spring boot with spring MVC

--- springboot/src/main/java/com/simplilearn/springboot/controller/MyController.java
+++ springboot/src/main/java/com/simplilearn/springboot/controller/MyController.java
@@ -1,7 +1,12 @@
 package com.simplilearn.springboot.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -9,12 +14,27 @@ public class MyController {
 	
 	
 	
-	@RequestMapping("/hello")
+	@RequestMapping(value="/get",method = RequestMethod.GET) // Mapping Handler
 	@ResponseBody
 	public String  getMessage() { //Model and View
 		
-		return "Hello friends";
+		return "Hello friends from Get Request";
 		
 	}
+	
+	@RequestMapping(value = "/post",method = RequestMethod.POST  )
+	//@ResponseBody
+	public String  getData(HttpServletRequest request,HttpServletResponse response,HttpSession session)  {
+		
+			String uname =	request.getParameter("uname");
+		
+			
+				session.setAttribute("uname", uname);
+				
+				return "success";  // view name by default prefix view folder , suffix is .jsp
+ 		
+	}
+	
+	
 
 }
