0223aafc20bd40daad5240d003a690e72291da21
javeed512 <javeedmohammedjaveed@gmail.com>
2022-03-29 15:38:23 +0530

    All Test Demos from JUNit5

--- rest_crud/src/main/java/com/simplilearn/ems/RestCrudApplication.java
+++ rest_crud/src/main/java/com/simplilearn/ems/RestCrudApplication.java
@@ -5,14 +5,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+import lombok.extern.java.Log;
+import lombok.extern.log4j.Log4j;
+import lombok.extern.slf4j.Slf4j;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
+@Slf4j
 public class RestCrudApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestCrudApplication.class, args);
+		
 	}
 	
 	
